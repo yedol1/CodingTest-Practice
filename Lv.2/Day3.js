f9625c18c9c27e3f059f2cfd73af3b682f858975
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-01-28 02:00:20 +0900

    solve: 23/01/28 문제풀이 완료

---
+++ Lv.2/Day3.js
@@ -0,0 +1,11 @@
+function solution(num) {
+  let answer = 0;
+
+  for (i = 1; i <= num; i++) {
+    // 맞는지는 모르겠으나, 약수 중에 홀수인 경우는 전부 연속된 수로 표현가능하다.
+    if (num % i == 0 && i % 2 == 1) answer++;
+  }
+  return answer;
+}
+
+console.log(solution(24));

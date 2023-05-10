c8d3d591984d259b2531c34ff6ee62b4f6dfc76c
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-05-10 21:28:42 +0900

    solve: 스택을 활용한 쇠막대기 문제풀이

---
+++ lec/stackQueue/solution5.js
@@ -0,0 +1,24 @@
+function solution(string) {
+  let open = "(";
+  let close = ")";
+  let openStack = [];
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === open) {
+      openStack.push(string[i]);
+    }
+    if (string[i] === close) {
+      //레이저
+      if (string[i - 1] === open) {
+        openStack.pop();
+        count += openStack.length;
+      } else {
+        //하나의 선분 끝
+        openStack.pop();
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
+console.log(solution("()(((()())(())()))(())"));

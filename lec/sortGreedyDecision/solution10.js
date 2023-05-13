575dfaf5b48dae27efe53fc218285a5757fc0a48
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-05-13 22:03:43 +0900

    solve: 이분탐색 문제풀이

---
+++ lec/sortGreedyDecision/solution10.js
@@ -0,0 +1,18 @@
+function solution(arr, target) {
+  arr.sort((a, b) => a - b);
+  let lp = 0;
+  let rp = arr.length - 1;
+  let answer = 0;
+  while (lp <= rp) {
+    let mid = Math.floor((lp + rp) / 2);
+    if (target === arr[mid]) {
+      return (answer = mid + 1);
+    } else if (target > arr[mid]) {
+      lp = mid + 1;
+    } else {
+      rp = mid - 1;
+    }
+  }
+  return answer;
+}
+console.log(solution([23, 87, 65, 12, 57, 32, 99, 81], 32));

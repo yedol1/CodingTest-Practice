adb9a7853d96198209cdfbab348da066c31095a5
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-05-25 17:36:43 +0900

    solve: 그리디 파트 문제 복기

---
+++ lec/sortGreedyDecision/solution11-2.js
@@ -0,0 +1,26 @@
+function solution(arr, dvdCount) {
+  let lp = Math.max(...arr);
+  let rp = arr.reduce((a, b) => a + b, 0);
+  let answer;
+  while (lp <= rp) {
+    let mid = Math.floor((lp + rp) / 2);
+    let count = 0;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (sum + arr[i] > mid || i === 0) {
+        sum = arr[i];
+        count++;
+      } else {
+        sum += arr[i];
+      }
+    }
+    if (count <= dvdCount) {
+      answer = mid;
+      rp = mid - 1;
+    } else {
+      lp = mid + 1;
+    }
+  }
+  return answer;
+}
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
---
+++ lec/sortGreedyDecision/solution12-2.js
@@ -0,0 +1,2 @@
+function solution(arr, count) {}
+console.log(solution([1, 2, 8, 4, 9], 3));
